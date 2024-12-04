6f22baa8462d6a5c466889c0a1633af4b40852e7
Kevin Lim <zzkevinlim@gmail.com>
2024-12-05 03:39:10 +1300

    20241205033910

--- header.php
+++ header.php
@@ -221,5 +221,12 @@ $menu_items = menu_build_tree(wp_get_nav_menu_items(get_nav_menu_locations()['he
 					<?php }	?>
 				</ul>
 			</nav>
+			<div class="flex lg:hidden justify-center ml-auto">
+				<span>
+					<svg class="size-10 rotate-180 fill-black cursor-pointer transition-all hover:fill-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+						<path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75H12a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+					</svg>
+				</span>
+			</div>
 		</div>
 	</header>
