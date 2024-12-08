08c9cdee776bf2f8b5afc92edfd0317b4cc00e31
Kevin Lim <zzkevinlim@gmail.com>
2024-12-08 15:26:26 +1300

    20241208152626

--- templates/header-default.php
+++ templates/header-default.php
@@ -108,12 +108,12 @@
 			</svg>
 		</a>
 		<nav class="h-full hidden lg:flex basis-full items-center gap-x-8">
-			<?php if(get_field('company_phone_number', 'option')) { ?>
-			<a href="tel:<?php echo get_field('company_phone_number', 'option')['url']; ?>" aria-label="<?php echo get_field('company_phone_number', 'option')['url']; ?>" target="<?php echo get_field('company_phone_number', 'option')['target']; ?>" class="relative shrink-0 w-11 h-11 border-solid border-3 border-black rounded-full transition-all hover:bg-green-500 hover:border-green-500 hover:scale-110 group">
-				<svg class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-6 fill-black transition-all group-hover:fill-white group-hover:-rotate-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-					<path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
-				</svg>
-			</a>
+			<?php if (get_field('company_phone_number', 'option')) { ?>
+				<a href="tel:<?php echo get_field('company_phone_number', 'option')['url']; ?>" aria-label="<?php echo get_field('company_phone_number', 'option')['url']; ?>" target="<?php echo get_field('company_phone_number', 'option')['target']; ?>" class="relative shrink-0 w-11 h-11 border-solid border-3 border-black rounded-full transition-all hover:bg-green-500 hover:border-green-500 hover:scale-110 group">
+					<svg class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-6 fill-black transition-all group-hover:fill-white group-hover:-rotate-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+						<path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
+					</svg>
+				</a>
 			<?php } ?>
 			<ul class="header-menu">
 				<?php foreach ($menu_items as $menu_item_index => $menu_item) { ?>
@@ -185,8 +185,8 @@
 					<?php echo get_field('header_button_link', 'option')['title']; ?>
 				</a>
 				<a href="<?php echo get_field('header_button_link_2', 'option')['url']; ?>" aria-label="<?php echo get_field('header_button_link_2', 'option')['title']; ?>" target="<?php echo get_field('header_button_link_2', 'option')['target']; ?>" class="inline-flex justify-center items-center gap-x-1 font-sans font-semibold text-base text-green-500 text-nowrap no-underline min-w-36 bg-transparent px-4 py-2 border-solid border-2 border-green-500 rounded-md transition-all hover:text-white hover:bg-green-500 hover:border-green-500 group hover:shadow-md">
-					<svg class="size-4 stroke-2 stroke-green-500 group-hover:stroke-white group-hover:fill-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+					<svg class="size-4 fill-green-500 group-hover:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+						<path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
 					</svg>
 					<?php echo get_field('header_button_link_2', 'option')['title']; ?>
 				</a>
@@ -328,12 +328,12 @@
 			</div>
 			<div class="flex items-center gap-x-4 basis-full px-4 md:px-8 pb-4">
 				<?php get_search_form(); ?>
-				<?php if(get_field('company_phone_number', 'option')) { ?>
-				<a href="tel:<?php echo get_field('company_phone_number', 'option')['url']; ?>" aria-label="<?php echo get_field('company_phone_number', 'option')['url']; ?>" target="<?php echo get_field('company_phone_number', 'option')['target']; ?>" class="relative shrink-0 w-11 h-11 border-solid border-3 border-black rounded-full transition-all hover:bg-green-500 hover:border-green-500 hover:scale-110 group">
-					<svg class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-6 fill-black transition-all group-hover:fill-white group-hover:-rotate-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-						<path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
-					</svg>
-				</a>
+				<?php if (get_field('company_phone_number', 'option')) { ?>
+					<a href="tel:<?php echo get_field('company_phone_number', 'option')['url']; ?>" aria-label="<?php echo get_field('company_phone_number', 'option')['url']; ?>" target="<?php echo get_field('company_phone_number', 'option')['target']; ?>" class="relative shrink-0 w-11 h-11 border-solid border-3 border-black rounded-full transition-all hover:bg-green-500 hover:border-green-500 hover:scale-110 group">
+						<svg class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-6 fill-black transition-all group-hover:fill-white group-hover:-rotate-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+							<path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
+						</svg>
+					</a>
 				<?php } ?>
 			</div>
 			<ul class="mobile-menu-container">
@@ -429,8 +429,8 @@
 					<?php echo get_field('header_button_link', 'option')['title']; ?>
 				</a>
 				<a href="<?php echo get_field('header_button_link_2', 'option')['url']; ?>" aria-label="<?php echo get_field('header_button_link_2', 'option')['title']; ?>" target="<?php echo get_field('header_button_link_2', 'option')['target']; ?>" class="inline-flex justify-center items-center gap-x-1 font-sans font-semibold text-base text-green-500 text-nowrap no-underline min-w-36 bg-transparent px-4 py-2 border-solid border-2 border-green-500 rounded-md transition-all hover:text-white hover:bg-green-500 hover:border-green-500 group hover:shadow-md">
-					<svg class="size-4 stroke-2 stroke-green-500 group-hover:stroke-white group-hover:fill-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+					<svg class="size-4 fill-green-500 group-hover:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+						<path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
 					</svg>
 					<?php echo get_field('header_button_link_2', 'option')['title']; ?>
 				</a>
