bfe228cd10552c7df869bfe079ffa9a8f3f04eb8
Kevin Lim <zzkevinlim@gmail.com>
2024-12-09 17:15:23 +1300

    20241209171523

--- templates/header-top-banner-2.php
+++ templates/header-top-banner-2.php
@@ -1,7 +1,7 @@
 <div class="hidden lg:block sticky z-10 top-0 w-full shadow-sm">
 	<div class="bg-orange-500">
 		<div class="container mx-auto px-4">
-			<div class="font-sans font-medium text-normal text-white text-center py-2">
+			<div class="font-sans font-semibold text-normal text-white text-center py-2">
 				<?php
 				echo $args['paragraph_before'];
 				echo get_field('company_currency', 'option') . get_field('company_payment_plan_price_per_week', 'option');
@@ -18,7 +18,7 @@
 	</div>
 	<div class="bg-white">
 		<div class="container mx-auto px-4 flex justify-center items-center gap-x-4">
-			<div class="font-sans font-medium text-normal text-black bg-white py-2">
+			<div class="font-sans font-semibold text-normal text-black bg-white py-2">
 				<?php echo $args['paragraph_2_before'] . $args['paragraph_2_during'] . $args['paragraph_2_after']; ?>
 			</div>
 			<a href="<?php echo $args['button_link']['url']; ?>" aria-label="<?php echo $args['button_link']['title']; ?>" target="<?php echo $args['button_link']['target']; ?>" class="inline-flex justify-center items-center gap-x-1 font-sans font-semibold text-base text-white text-nowrap min-w-36 bg-green-500 px-4 py-0 border-solid border-2 border-green-500 rounded-md transition-all hover:text-white hover:bg-orange-400 hover:border-orange-400 hover:shadow-md">
