94f18a847d18dd1ce750995482ba086ee8408ac5
Kevin Lim <zzkevinlim@gmail.com>
2024-12-09 15:47:36 +1300

    20241209154736

--- footer.php
+++ footer.php
@@ -3,7 +3,15 @@ if (check_render_condition('footer')) {
 	get_template_part('templates/footer', 'default');
 }
 ?>
-<?php wp_footer(); ?>
+<?php
+wp_footer();
+
+foreach (get_field('body_close_scripts', 'option') as $body_close_script) {
+	if ($body_close_script['enabled']) {
+		echo $body_close_script['script'];
+	}
+}
+?>
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -2,15 +2,36 @@
 <html <?php language_attributes(); ?> class="h-full">
 
 <head>
+	<?php
+	foreach (get_field('head_open_scripts', 'option') as $head_open_script) {
+		if ($head_open_script['enabled']) {
+			echo $head_open_script['script'];
+		}
+	}
+	?>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<?php wp_head(); ?>
+	<?php
+	wp_head();
+
+	foreach (get_field('head_close_scripts', 'option') as $head_close_script) {
+		if ($head_close_script['enabled']) {
+			echo $head_close_script['script'];
+		}
+	}
+	?>
 </head>
 
 <body <?php body_class(print_debug_screens_if_in_development_mode() . ' h-full'); ?>>
 	<?php
 	wp_body_open();
+
+	foreach (get_field('body_open_scripts', 'option') as $body_open_script) {
+		if ($body_open_script['enabled']) {
+			echo $body_open_script['script'];
+		}
+	}
 
 	foreach (get_field('header_top_banners', 'option') as $header_top_banner_index => $header_top_banner) {
 		if ($header_top_banner['enabled'] && check_render_condition_for_flexible_content($header_top_banner['acf_fc_layout'] . '_' . ++$header_top_banner_index, $header_top_banner['include_pages'], $header_top_banner['include_categories'], $header_top_banner['include_product_categories'], $header_top_banner['include_subject_categories'], $header_top_banner['exclude_pages'], $header_top_banner['exclude_categories'], $header_top_banner['exclude_product_categories'], $header_top_banner['exclude_subject_categories'])) {
@@ -31,14 +52,16 @@
 			get_template_part('templates/' . str_replace('_', '-', $header_bottom_banner['acf_fc_layout']), null, $header_bottom_banner);
 		}
 	}
+
+	$popups = get_field('popups', 'option');
 	?>
 
 	<script>
-		window.popups = !!<?php echo json_encode(get_field('popups', 'option')); ?>.length;
+		window.popups = !!<?php echo json_encode($popups); ?>.length;
 	</script>
 
 	<?php
-	foreach (get_field('popups', 'option') as $popup_index => $popup) {
+	foreach ($popups as $popup_index => $popup) {
 		if ($popup['enabled'] && check_render_condition_for_flexible_content($popup['acf_fc_layout'] . '_' . ++$popup_index, $popup['include_pages'], $popup['include_categories'], $popup['include_product_categories'], $popup['include_subject_categories'], $popup['exclude_pages'], $popup['exclude_categories'], $popup['exclude_product_categories'], $popup['exclude_subject_categories'])) {
 			get_template_part('templates/' . str_replace('_', '-', $popup['acf_fc_layout']), null, $popup);
 		}
