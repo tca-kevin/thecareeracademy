ecd1f89d5cb2fe202490f87a911e999ebbc6a5a0
Kevin Lim <zzkevinlim@gmail.com>
2024-12-11 18:56:14 +1300

    20241211185614

--- templates/header-bottom-banner-2.php
+++ templates/header-bottom-banner-2.php
@@ -1,5 +1,5 @@
 <div class="container mx-auto px-4 pt-2">
-	<a href="<?php echo $args['image_link']['url']; ?>" aria-label="<?php echo $args['image_link']['title']; ?>" target="<?php echo $args['image_link']['target']; ?>" class="block" style="height: <?php echo $args['image_file']['height']; ?>px;">
+	<a href="<?php echo $args['image_link']['url']; ?>" aria-label="<?php echo $args['image_link']['title']; ?>" target="<?php echo $args['image_link']['target']; ?>">
 		<img data-src="<?php echo $args['image_file']['url']; ?>" alt="<?php echo $args['image_file']['alt']; ?>" width="<?php echo $args['image_file']['width']; ?>" height="<?php echo $args['image_file']['height']; ?>" class="lazy object-contain 2xl:object-none object-center w-full 2xl:w-auto h-full 2xl:h-auto" />
 	</a>
 </div>
